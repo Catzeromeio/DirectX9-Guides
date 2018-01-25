3e2b95745affe1b7731c5226632835b19da1dce8
catzeromeio <catzeromeio@hotmail.com>
2018-01-25 14:39:47 +0800

    finally init sucessfully

--- DirectX9-Guides/D3DUtility.h
+++ DirectX9-Guides/D3DUtility.h
@@ -12,6 +12,7 @@ namespace d3d
 
 		D3DCAPS9 caps;
 		_d3d9->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps);
+		int count = _d3d9->GetAdapterCount();
 
 		int vp = 0;
 		if (caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
@@ -20,37 +21,75 @@ namespace d3d
 			vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;
 
 		D3DPRESENT_PARAMETERS d3dpp;
+
+		ZeroMemory(&d3dpp, sizeof(d3dpp));
+
 		d3dpp.BackBufferWidth = 800;
 		d3dpp.BackBufferHeight = 600;
-		d3dpp.BackBufferFormat = D3DFMT_A8B8G8R8;
+		//d3dpp.BackBufferFormat = D3DFMT_X8B8G8R8;
 		d3dpp.BackBufferCount = 1;
 		d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
 		d3dpp.MultiSampleQuality = 0;
-		d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+		d3dpp.SwapEffect = D3DSWAPEFFECT_COPY;
 		d3dpp.hDeviceWindow = hwnd;
-		d3dpp.Windowed = false;
-		d3dpp.EnableAutoDepthStencil = true;
-		d3dpp.AutoDepthStencilFormat = D3DFMT_D24FS8;
-		d3dpp.Flags = 0;
+		d3dpp.Windowed = true;
+		d3dpp.EnableAutoDepthStencil = false;
+		d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 		d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
 		d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
 
 		HRESULT hr = _d3d9->CreateDevice(
-			0,
+			D3DADAPTER_DEFAULT,
 			D3DDEVTYPE_HAL,
 			hwnd,
-			vp,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING,
 			&d3dpp,
 			&_device
 		);
 
+
 		if (FAILED(hr))
 		{
-			const wchar_t contents[] = L"create device failed";
 			const wchar_t cap[] = L"error";
-			::MessageBox(hwnd, contents, cap, 0);
+
+			switch (hr)
+			{
+			case D3DERR_DEVICELOST:
+			{
+				const wchar_t contents[] = L"DEVICELOST";
+				::MessageBox(hwnd, contents, cap, 0);
+				break;
+			}
+			case D3DERR_INVALIDCALL:
+			{
+				const wchar_t contents[] = L"INVALIDCALL";
+				::MessageBox(hwnd, contents, cap, 0);
+				break;
+			}
+			case D3DERR_NOTAVAILABLE:
+			{
+				const wchar_t contents[] = L"NOTAVAILABLE";
+				::MessageBox(hwnd, contents, cap, 0);
+				break;
+			}
+			case D3DERR_OUTOFVIDEOMEMORY:
+			{
+				const wchar_t contents[] = L"OUTOFVIDEOMEMORY";
+				::MessageBox(hwnd, contents, cap, 0);
+				break;
+			}
+			default:
+			{
+				const wchar_t contents[] = L"create device failed";
+				::MessageBox(hwnd, contents, cap, 0);
+				break;
+			}
+			}
+
 			return 0;
 		}
+
+		return true;
 	}
 
 	bool Display()
